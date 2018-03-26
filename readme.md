dd528b7528e07faa7809a490c06b43546334ae18
Sergii Tychkin <sergiy.tychkin@gmail.com>
2018-03-26 19:00:20 +0300

    REST API spring

---
+++ readme.md
@@ -0,0 +1,13 @@
+Project for
+
+REST Endpoint	
+
+
+
+HTTP Method	Description
+
+/customers	GET	Returns the list of customers
+/customers/{id}	GET	Returns customer detail for given customer {id}
+/customers	POST	Creates new customer from the post data
+/customers/{id}	PUT	Replace the details for given customer {id}
+/customers/{id}	DELETE	Delete the customer for given customer {id}
---
+++ src/main/java/ua/tsv/restful/config/WebConfig.java
@@ -0,0 +1,15 @@
+package ua.tsv.restful.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(value = "ua.tsv.restful")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+
+}
